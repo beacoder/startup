c13ac14e8ed31956d0d65a0dc26345c837fad630
chm <chenhuming@gmail.com>
2013-06-02 19:33:56 +0800

    add proxy

--- AutoExec.bat
+++ AutoExec.bat
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # 开机启动程序
-
 # Command Line
-/bin/bash 
-
-# Emacs
-emacs &
+/bin/bash &
 
 # Goagent
 python /home/chm/Goagent/proxy.py &
+
+# Emacs uses Goagent proxy
+http_proxy=http://127.0.0.1:8087 emacs &
 
 # Chrome
 /usr/lib/chromium-browser/chromium-browser www.csdn.net https://plus.google.com planet.emacsen.org blog.binchen.org http://www.youtube.com &

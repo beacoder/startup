b7e46f5571aad1f621a289aa289d1be5dc9ed18e
陈虎明 <chenhuming@gmail.com>
2013-08-26 12:43:40 +0800

    Create StartUp.bat

---
+++ StartUp.bat
@@ -0,0 +1,8 @@
+# 开机启动程序
+
+# start chrome
+start "" chrome www.csdn.net https://plus.google.com planet.emacsen.org blog.binchen.org http://www.youtube.com http://mail.huatek.com
+
+# start outlook
+start "" outlook
+

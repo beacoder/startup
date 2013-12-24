a70bb58a246f30d0f539212fbf784a16ae33ea6a
陈虎明 <chenhuming@gmail.com>
2013-12-25 00:06:35 +0800

    Update StartUp.bat

--- StartUp.bat
+++ StartUp.bat
@@ -1,7 +1,7 @@
 # windows 开机启动程序
 
 # start chrome
-start "" chrome www.csdn.net https://plus.google.com planet.emacsen.org blog.binchen.org http://www.youtube.com http://mail.huatek.com http://lifehacker.com
+start "" https://app.yinxiang.com/Home.action https://kiwiirc.com/client chrome www.csdn.net https://plus.google.com planet.emacsen.org blog.binchen.org http://www.youtube.com http://mail.huatek.com http://lifehacker.com
 
 # start outlook
 start "" outlook

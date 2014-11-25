e4256f6a332a6530b40668f671f2ebfbcfaefa63
陈虎明 <chenhuming@gmail.com>
2014-11-25 09:23:55 +0800

    Update StartUp.bat

--- StartUp.bat
+++ StartUp.bat
@@ -3,6 +3,9 @@
 # start chrome
 start "" chrome https://app.yinxiang.com/Home.action https://kiwiirc.com/client www.csdn.net https://plus.google.com planet.emacsen.org blog.binchen.org http://www.youtube.com http://mail.huatek.com http://lifehacker.com
 
+# start IE
+start iexplore.exe -new www.csdn.net
+start iexplore.exe -new planet.emacsen.org
+
 # start outlook
 start "" outlook
-
